b0c14f24e9771f8ecd27413dc0aa89ecc14c94c1
zupercool <karolin.gjothlen.8133@student.uu.se>
2017-09-27 11:12:16 +0200

    added the spaceship to the screen

--- source/maingamestate.js
+++ source/maingamestate.js
@@ -5,11 +5,17 @@ var mainGameState = { };
 mainGameState.preload = function () {
     console.log("Pre-loading the Game");
     game.load.image('space-bg', 'assets/images/space-bg.jpg');
+    game.load.image('player-ship', 'assets/images/player-ship.png');
 };
 
 // Add the create function
 mainGameState.create = function () {
     game.add.sprite(0, 0, 'space-bg');
+    var shipX = game.width * 0.5;
+    var shipY = game.height * 0.9;
+    this.playerShip = game.add.sprite(shipX, shipY, 'player-ship');
+    this.playerShip.scale.setTo(0.2, 0.2);
+    this.playerShip.anchor.setTo(0.5, 0.5);
 };
 
 // Add the update function
